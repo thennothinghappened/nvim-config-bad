3ba5505bd3cc6980399d1a92b9e9ac3d524b4dcf
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-08-13 11:14:24 +1000

    open nvim-tree on dir open

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,11 @@
 return {
     'nvim-tree/nvim-tree.lua',
     opts = {
+        disable_netrw = true,
+        -- open_on_setup = true,
         diagnostics = {
             enable = true,
             show_on_dirs = false
         }
     },
-    lazy = true,
-    cmd = { 'NvimTreeToggle' },
 }
