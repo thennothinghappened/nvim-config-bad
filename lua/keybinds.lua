7a10ab24dc50e986c5198af7469aebade67062b1
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-09-09 17:26:24 +1000

    add LSP format option for right click

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -29,9 +29,15 @@ local function rightclick()
     vim.cmd.aunmenu { 'PopUp.How-to\\ disable\\ mouse' }
 
     -- Add our items
+
+    -- LSP references
     vim.cmd.anoremenu { 'PopUp.Goto\\ Definition', ':lua vim.lsp.buf.definition()<CR>' }
     vim.cmd.anoremenu { 'PopUp.References', ':lua vim.lsp.buf.references()<CR>' }
     vim.cmd.anoremenu { 'PopUp.Rename', ':lua vim.lsp.buf.rename()<CR>' }
+
+    -- LSP format file
+    vim.cmd.anoremenu { 'PopUp.-2-', '<Nop>' }
+    vim.cmd.anoremenu { 'PopUp.Format\\ file', ':lua vim.lsp.buf.format()<CR>' }
 end
 
 --- Neovide-specific (fix MacOS copy/paste)
@@ -48,12 +54,15 @@ local function neovide()
     end
 end
 
+--- Keybinds for nvim-tree
+local function nvim_tree()
+    -- Toggle tree in normal mode
+    vim.keymap.set('n', 'T', '<Cmd>NvimTreeToggle<CR>', { silent = true, nowait = true })
+end
+
 return {
     -- Keybinds for nvim-tree
-    nvim_tree = function()
-        -- Toggle tree in normal mode
-        vim.keymap.set('n', 'T', '<Cmd>NvimTreeToggle<CR>', { silent = true, nowait = true })
-    end,
+    nvim_tree = nvim_tree,
 
     -- General keybinds
     general = general,
