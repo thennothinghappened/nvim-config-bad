9927ef9d34806040415261fb7942c65d9886b6a9
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-09-09 16:32:43 +1000

    refactor keybinds, add mac command+z

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,33 +1,38 @@
--- Keybind options
-local function opts(desc)
-    return {
-        desc = desc,
-        buffer = buf_number,
-        noremap = true,
-        silent = true,
-        nowait = true
-    }
-end
+local os = require('helper').os
 
--- General keybinds
+--- General keybinds
 local function general()
+    -- Set wrap behaviour for arrowing around
     vim.go.whichwrap = 'b,s,<,>,[,]'
+
+    -- Blasphemy
+    if os.macOS then
+        -- Command-Z undo
+        vim.keymap.set({ '', 't' }, '<D-z>', 'u', { silent = true })
+        vim.keymap.set('i', '<D-z>', '<C-o>:u<CR>', { silent = true })
+
+        -- Command-Shift-Z redo
+        -- TODO: this doesn't work :(
+        vim.keymap.set('', '<D-S-v>', '<C-r>', { silent = true })
+        vim.keymap.set('!', '<D-S-v>', '<C-o>:<C-r><CR>', { silent = true })
+    end
 end
 
--- Neovide-specific (fix MacOS paste)
+--- Neovide-specific (fix MacOS paste)
 local function neovide()
-    --vim.keymap.set('', '<D-v>', '+p<CR>', opts('Neovide: MacOS clipboard'))
-    vim.g.neovide_input_use_logo = 1
-    vim.api.nvim_set_keymap('', '<D-v>', 'p<CR>', { noremap = true, silent = true})
-    vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-    vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-    vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+    if os.macOS then
+        vim.g.neovide_input_use_logo = 1
+
+        vim.keymap.set('', '<D-v>', 'p<CR>', { silent = true, nowait = true })
+        vim.keymap.set({ '!', 'v' }, '<D-v>', '<C-R>+', { silent = true, nowait = true })
+    end
 end
 
 return {
     -- Keybinds for nvim-tree
-    nvim_tree = function ()
-        vim.keymap.set('n', 'T', '<Cmd>NvimTreeToggle<CR>', opts('Nvim-Tree: Toggle Tree'))
+    nvim_tree = function()
+        -- Toggle tree in normal mode
+        vim.keymap.set('n', 'T', '<Cmd>NvimTreeToggle<CR>', { silent = true, nowait = true })
     end,
 
     -- General keybinds
